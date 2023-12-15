11e549b0d3705be86aab319665cbec4ddb844d19
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-15 20:45:47 +0530

    Feat: experience section layout added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/Section-Divider";
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills/>
+      <Experience/>
     </main>
   );
 }
