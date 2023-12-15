8f72d707bb5966641bf8333f6a8fe4a139d4b64c
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-15 19:28:57 +0530

    Feat: add custom hook with section types

---
+++ lib/hooks.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { SectionName } from "./types";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+  const { ref, inView } = useInView({
+    threshold,
+  });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection(sectionName);
+    }
+  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
+
+  return {
+    ref,
+  };
+}
