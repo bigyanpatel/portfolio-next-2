c10ed042f064238dd222859fae1dbcd582066718
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-15 19:54:57 +0530

    Feat: active state change on scroll

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,10 +7,14 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+
   return (
     <section
+      ref={ref}
       id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import { projectsData } from "@/lib/data";
 import SectionHeading from "./Section-Heading";
 import Project from "./Project";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.5);
 
   return (
-    <section id="projects" className="scroll-mt-28 mb-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
