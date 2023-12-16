bab8a957cce627c66e1d937a7e83523d227efadb
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-16 23:23:42 +0530

    Fix: Experience section not visible

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,48 +1,23 @@
 "use client";
 
 import React from "react";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+import { VerticalTimeline } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import SectionHeading from "./Section-Heading";
+import VerticalTimelineComponent from "./VerticalTimelineComponent";
 
 export default function Experience() {
-  const { ref } = useSectionInView("Experience");
-    
+  const { ref } = useSectionInView("Experience", 0.2);
+
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
-            <VerticalTimelineElement
-              contentStyle={{
-                background: "rgba(255, 255, 255, 0.05)",
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{
-                borderRight: "0.4rem solid rgba(255, 255, 255, 0.5)",
-              }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{
-                background: "rgba(255, 255, 255, 0.15)",
-                fontSize: "1.5rem",
-              }}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
+            <VerticalTimelineComponent item={item} />
           </React.Fragment>
         ))}
       </VerticalTimeline>
---
+++ components/VerticalTimelineComponent.tsx
@@ -0,0 +1,60 @@
+import { useTheme } from "@/context/theme-context";
+import { ExperienceData } from "@/lib/types";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+
+type VerticalTimelineElementProps = {
+  item: ExperienceData;
+};
+
+const VerticalTimelineComponent = ({ item }: VerticalTimelineElementProps) => {
+  const { theme } = useTheme();
+
+  const [visible, setVisible] = useState(false);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setVisible(true);
+    }
+  }, [inView]);
+
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+        boxShadow: "none",
+        border: "1px solid rgba(0, 0, 0, 0.05)",
+        textAlign: "left",
+        padding: "1.3rem 2rem",
+      }}
+      contentArrowStyle={{
+        borderRight:
+          theme === "light"
+            ? "0.4rem solid #9ca3af"
+            : "0.4rem solid rgba(255, 255, 255, 0.5)",
+      }}
+      date={item.date}
+      icon={item.icon}
+      iconStyle={{
+        background: theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+        fontSize: "1.5rem",
+      }}
+      visible={visible}
+    >
+      <h3 ref={ref} className="font-semibold capitalize">
+        {item.title}
+      </h3>
+      <p className="font-normal !mt-0">{item.location}</p>
+      <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+        {item.description}
+      </p>
+    </VerticalTimelineElement>
+  );
+};
+
+export default VerticalTimelineComponent;
