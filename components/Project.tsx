92ccff169ff7ff7f1d96934d60e4c1879f9bef4e
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-18 08:41:32 +0530

    Style: images in the card

--- components/Project.tsx
+++ components/Project.tsx
@@ -52,7 +52,7 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          className="absolute hidden sm:block top-20 -right-40 w-[30rem] rounded-t-lg shadow-2xl
         transition 
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
